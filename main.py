15fd975d0de0d6546607e55b22fe54960a020645
Ana Luiza <analuizasantosnascimento4@gmail.com>
2025-09-26 11:17:19 -0300

    adicionar alunos a tabela escola.db

--- main.py
+++ main.py
@@ -8,11 +8,40 @@ conexao = sqlite3.connect("escola.db")
 cursor = conexao.cursor()
 
 #Criar uma tabela no banco
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS alunos (
-    id INTEGER  PRIMARY KEY AUTOINCREMENT,
-    nome TEXT NOT NULL,
-    idade INTEGER,
-    curso TEXT                
-   )               
-""")
+# cursor.execute("""
+# CREATE TABLE IF NOT EXISTS alunos (
+#     id INTEGER  PRIMARY KEY AUTOINCREMENT,
+#     nome TEXT NOT NULL,
+#     idade INTEGER,
+#     curso TEXT                
+#    )               
+# """)
+# nome = input("Digite o nome do aluno: ").lower()
+# idade = int(input("Digite aideia do aluno: "))
+# curso = input("Digite o curso do aluno: ")
+# # Inseror um dado na tabela 
+# cursor.execute("""
+# INSERT INTO alunos (nome,idade,curso)
+# VALUES (?,?,?)               
+# """,
+# (nome,idade,curso)
+# )
+
+# #Confirmar as alterações no banco 
+# conexao.commit()
+
+
+#Inserir varios alunos de uma só vez
+alunos = [
+    ("Yago",28,"Direito"),
+    ("Jessica",24,"computação"),
+    ("Breno",52,"Computação"),
+]
+# executemany permite inserir multiplas linhas de uma so vez
+cursor.executemany("""
+INSERT INTO alunos (nome,idade,curso)
+VALUES (?,?,?)                   
+ """,
+(alunos)
+)
+conexao.commit()
